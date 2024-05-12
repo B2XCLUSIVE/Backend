d3b73a55ec5d39e733f6eef8ef85cf7b7052092c
IgnatiusFrancis <ignatiuzzfrank@gmail.com>
2024-05-12 13:38:37 +0100

    Feat: Removed app controller bug

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://b2exclusive.vercel.app'],
+    origin: ['http://localhost:3000', 'https://b2xclusive.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,PATCH',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
